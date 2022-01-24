ded9f0a9a0ef84d1723dbc4fa109dc6a41ee3b52
ksvedal <kristoffersvedal@outlook.com>
2022-01-24 12:18:42 +0100

    added multithreading and split the value to calculate equally among threads

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,62 @@
-use std::io;
+use std::thread;
+
 fn main() {
-    let mut input = String::new();
-    println!("Starting number");
-    io::stdin().read_line(&mut input).unwrap();
-    let start: i64 = input.trim().parse().unwrap();
+    // let start: f64 = 0;
+    let end: i64 = 1000;
+    let nthreads: i64 = 20;
+    let range: i64 = end / nthreads;
 
-    let mut input = String::new();
-    println!("End number");
-    io::stdin().read_line(&mut input).unwrap();
-    let end: i64 = input.trim().parse().unwrap();
+    let mut all_primes : Vec<i64> = Vec::new();
     
-    let mut found: i64 = 0;
+    let mut threads = Vec::new();
 
-    for count in start..end {
-        if count % 2 != 0 {        
-            // If prime check passes, increment found and print count.
-            if check_prime(&count) { 
-                found += 1;
-                println!("{}",&count);
+    for i in 0..nthreads {
+        if i == 0 {
+            threads.push(thread::spawn(move || {
+                return prime_check(1 + (i*range), (i + 1)*range)
+            }));
+        } else {
+            if i == nthreads -1 {
+                threads.push(thread::spawn(move || {
+                    return prime_check(i*range, end)
+                }));
+            } else {
+                threads.push(thread::spawn(move || {
+                    return prime_check(i*range, (i+1)*range)
+                }));
             }
         }
     }
 
-    
-    println!("{}: {}","Prime numbers found",&found);
+    for thread in threads {
+        let result = thread.join().unwrap();
+        all_primes.extend(result);
+    }
 
+    println!("{:?}", all_primes);
+
+    fn prime_check(from: i64, to: i64) -> Vec<i64>{
+        let mut primes : Vec<i64> = Vec::new();
+
+        for count in from..to {
+            if count == 1 {
+                continue;
+            } else if count == 2 {
+                primes.push(count);
+            } else if count % 2 != 0 {        
+                // If prime check passes, increment found and print count.
+                if check_prime(&count) { 
+                    primes.push(count);
+                }
+            }  
+        }
+        // println!("{:?}",&primes);
+        return primes;
+    }
+    
     fn check_prime(count: &i64) -> bool { 
         // Find stopping number to loop to
-        let mut stop = ((*count as f64).sqrt() + 1.0) as i64;
+        let stop = ((*count as f64).sqrt() + 1.0) as i64;
         // Start at 3. Counts until stop value.
         for i in 3..stop {       
             // Check if i is even
@@ -38,6 +67,6 @@ fn main() {
                 }
             }
         }
-        return true
+        return true;
     }
 }
